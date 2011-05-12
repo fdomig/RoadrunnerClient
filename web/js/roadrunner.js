f396f9d75da0293e8ff041d32a127874150bcbea
Franziskus Domig <fdomig@gmail.com>
2011-05-12 16:33:56 +0200

    added roadrunner.js

---
+++ web/js/roadrunner.js
@@ -0,0 +1,9 @@
+jQuery(function() {
+
+	// add item to delivery on create
+	$('.add-item-to-delivery').click(function(e) {
+		e.preventDefault();
+		$('#item-list tbody').append('<tr><td>does not</td><td>work yet</td></tr>');
+	});
+	
+});
