65e298f2a52d8847656221164db5e81651f257cc
Franziskus Domig <fdomig@gmail.com>
2011-05-12 16:32:33 +0200

    added delivery controller

--- src/Roadrunner/Controller/DeliveryController.php
+++ src/Roadrunner/Controller/DeliveryController.php
@@ -2,6 +2,7 @@
 namespace Roadrunner\Controller;
 
 use Roadrunner\Model\Delivery;
+use Roadrunner\Model\Address;
 
 class DeliveryController extends BaseController {
 	
@@ -15,6 +16,36 @@ class DeliveryController extends BaseController {
 		return $this->render('delivery.list.twig', array(
 			'delivery_list' => Delivery::getAll($this->getDocumentManager()),
 		));
+	}
+	
+	public function executeView()
+	{
+		return $this->render('delivery.view.twig', array(
+			'delivery' => Delivery::find(
+				$this->getDocumentManager(),
+				$this->getRequest('id')
+			),
+		));
+	}
+	
+	public function executeAdd()
+	{
+		return $this->render('delivery.add.twig', array(
+			'form_action' => url_for('/delivery/create'),
+		));
+	}
+	
+	public function executeCreate()
+	{
+		$delivery = new Delivery();
+		$delivery->setFromAddress(new Address($this->getRequest('from')));
+		$delivery->setToAddress(new Address($this->getRequest('to')));
+		
+		$manager = $this->getDocumentManager();
+		$manager->persist($delivery);
+		$manager->flush();
+		
+		$this->redirect('/delivery/view/' . $delivery->getId());
 	}
 	
 }
