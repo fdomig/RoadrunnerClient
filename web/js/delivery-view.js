d48d48200f35352c2dd96126b261af864405b56f
Franziskus Domig <fdomig@gmail.com>
2011-05-18 16:56:54 +0200

    added google maps to delivery view

--- web/js/delivery-view.js
+++ web/js/delivery-view.js
@@ -4,18 +4,14 @@ jQuery(function() {
   var markersArray = [];
 
   function initialize() {
-    var haightAshbury = new google.maps.LatLng(47.413417, 9.744417);
+    var home = new google.maps.LatLng(47.413417, 9.744417);
     var mapOptions = {
       zoom: 12,
-      center: haightAshbury,
+      center: home,
       mapTypeId: google.maps.MapTypeId.TERRAIN
     };
     map = new google.maps.Map(document.getElementById("map_canvas"),
         mapOptions);
-
-    google.maps.event.addListener(map, 'click', function(event) {
-      addMarker(event.latLng);
-    });
   }
 
   function addMarker(location) {
