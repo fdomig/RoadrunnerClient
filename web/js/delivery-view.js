f99dab2a52e6eacd32b2e2e9d5e6ec34bac0c3c7
matthias <schmid_mat@gmx.at>
2011-06-06 23:45:58 +0200

    implemented show current position

--- web/js/delivery-view.js
+++ web/js/delivery-view.js
@@ -1,6 +1,7 @@
 jQuery(function() {
 	
 	var map;
+	var image;
 	var markersArray = [];
 	var directionDisplay;
 	var directionsService = new google.maps.DirectionsService();
@@ -15,19 +16,34 @@ jQuery(function() {
 		map = new google.maps.Map(document.getElementById("map_canvas"),
 			mapOptions
 		);
+		
+		image = new google.maps.MarkerImage('/img/marker_pos.png',
+				// This marker is 20 pixels wide by 32 pixels tall.
+				new google.maps.Size(20, 32),
+				// The origin for this image is 0,0.
+				new google.maps.Point(0,0),
+		      	// The anchor for this image is the base of the flagpole at 0,32.
+		      	new google.maps.Point(0, 32));
+		
 		directionsDisplay = new google.maps.DirectionsRenderer();
 		directionsDisplay.setMap(map);
-
 		$('.delivery-entry').each(function() {
 			drawDirections($(this).attr('id'));
+			drawPositions($(this).attr('id'));
 		});
 		
 	}
 
-	function addMarker(location) {
-		marker = new google.maps.Marker({
+	function addMarker(location, title) {
+		var marker = new google.maps.Marker({
 			position: location,
-			map: map
+			map: map,
+			icon: image,
+			title: title
+		});
+		google.maps.event.addListener(marker, 'click', function() {
+			var infowindow = new google.maps.InfoWindow({content: title});
+			infowindow.open(map,marker);
 		});
 		markersArray.push(marker);
 	}
@@ -65,13 +81,20 @@ jQuery(function() {
 			options = {
 				origin: route.origin,
 				destination: route.destination,
-				travelMode: google.maps.DirectionsTravelMode.DRIVING,
+				travelMode: google.maps.DirectionsTravelMode.DRIVING
 			}
 			directionsService.route(options, function(result, status) {
 				if (status == google.maps.DirectionsStatus.OK) {
 					directionsDisplay.setDirections(result);
 				}
 			});
+		});
+	}
+	
+	function drawPositions(id) {
+		$.getJSON('/delivery/positions/' + id, function(position) {
+			pos = new google.maps.LatLng(position.lat, position.lng);
+			addMarker(pos, "Current Position");
 		});
 	}
 
