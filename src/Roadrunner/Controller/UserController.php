6c417cdd6c1c474fea25eeaf7a5b2697bb342b48
Franziskus Domig <fdomig@gmail.com>
2011-06-22 21:18:50 +0200

    fixed PHPCS errors

--- src/Roadrunner/Controller/UserController.php
+++ src/Roadrunner/Controller/UserController.php
@@ -5,21 +5,25 @@ use Roadrunner\Model\User;
 
 class UserController extends BaseController {
 	
-	public function executeList() {	
+	public function executeList()
+	{	
 		return $this->render('user.list.twig', array(
 			'user_list' => User::getAll(),
 		));
 	}
 	
-	public function executeAdd()  {
+	public function executeAdd()
+	{
 		return $this->render('user.add.twig', array(
 			'form_action' => '/user/create',
 		));
 	}
-	public function executeCreate()  {			
-		$name = $this->getRequest()->get('name');
+	
+	public function executeCreate()
+	{			
+		$name     = $this->getRequest()->get('name');
 		$password = $this->getRequest()->get('password');
-		$roles = $this->getRequest()->get('roles');
+		$roles    = $this->getRequest()->get('roles');
 		
 		if (empty($name)) {
 			throw new \Exception("Name of the user is not set.");
